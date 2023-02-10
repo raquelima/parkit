14864660478f1a2ccd6defaba834266b30d4f834
Raquel Lima <rrlima.raquel@gmail.com>
2023-02-10 14:12:58 +0100

    provide client instance globally

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,37 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import { developmentBaseUrl } from "../mock/conf.js";
 import SwaggerClient from "swagger-client";
 import spec from "@berufsbildung-basel/parkit-spec/api.yml";
-import { developmentBaseUrl } from "../mock/conf.js";
+
+export const SwaggerClientContext = React.createContext();
 
 function App() {
-  const [serverMessage, setServerMessage] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      // TODO provide client instance globally
-      const client = await new SwaggerClient({
-        spec: {
-          ...spec,
-          servers: [
-            { url: `${developmentBaseUrl}/api`, description: "Test server" },
-          ],
-        },
-        requestInterceptor: (request) => {
-          Object.keys(localStorage).forEach((key) => {
-            if (key.startsWith("x-test-")) {
-              request.headers[key] = localStorage.getItem(key);
-            }
-          });
-          request.headers["authorization"] = `Basic ${btoa(
-            "test@adobe.com:testPassword"
-          )}`;
-          return request;
-        },
+  
+  const client = new SwaggerClient({
+    spec: {
+      ...spec,
+      servers: [
+        { url: `${developmentBaseUrl}/api`, description: "Test server" },
+      ],
+    },
+    requestInterceptor: (request) => {
+      Object.keys(localStorage).forEach((key) => {
+        if (key.startsWith("x-test-")) {
+          request.headers[key] = localStorage.getItem(key);
+        }
       });
-
-      client.apis.users
-        .listUsers()
-        .then((response) => {
-          if (response?.ok) {
-            setServerMessage(response.body?.users?.[0].email ?? "No results");
-          }
-        })
-        .catch((e) => {
-          setServerMessage(
-            `An error occurred: ${e.statusCode} - ${e.response?.statusText}`
-          );
-        });
-    })();
-  }, []);
+      request.headers["authorization"] = `Basic ${btoa(
+        "test@adobe.com:testPassword"
+      )}`;
+      return request;
+    },
+  });
 
   return (
-    <>
+    <SwaggerClientContext.Provider value={client}>
       <h1>Hello World</h1>
       {serverMessage && <p>{serverMessage}</p>}
-    </>
+    </SwaggerClientContext.Provider>
   );
 }
 
