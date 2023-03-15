0ff62537a9d3a845d27b227750cdd6cc02e942ff
Raquel Lima <rrlima.raquel@gmail.com>
2023-03-15 15:17:22 +0100

    implement react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import useCreateSwaggerClient from "./useCreateSwaggerClient.jsx";
+import AppRoutes from "./AppRoutes.jsx";
 
 export const SwaggerClientContext = React.createContext();
 
 function App() {
   const client = useCreateSwaggerClient();
-  const [users, setUsers] = useState(null);
-  const [serverMessage, setServerMessage] = useState(null);
-
-  useEffect(() => {
-    if (client?.apis?.users) {
-      (async () => {
-        client.apis.users
-          .listUsers()
-          .then((response) => {
-            if (response?.ok) {
-              setUsers(response.body.users);
-            }
-          })
-          .catch((e) => {
-            setServerMessage(
-              `An error occurred: ${e.statusCode} - ${e.response?.statusText}`
-            );
-          });
-      })();
-    }
-  }, [client]);
 
   return (
     <SwaggerClientContext.Provider value={client}>
-      <h1>Hello, </h1>
-      {serverMessage ? (
-        <p>{serverMessage}</p>
-      ) : (
-        users &&
-        users.map((user) => {
-          return <span key={user.id}>{user.username}</span>;
-        })
-      )}
+      <AppRoutes/>
     </SwaggerClientContext.Provider>
   );
 }
---
+++ src/components/Vehicles.jsx
@@ -0,0 +1,10 @@
+function Vehicles() {
+    return (
+      <div>
+        <p>Welcome to your Vehicles</p>
+              
+      </div>
+    );
+  }
+  
+  export default Vehicles;
