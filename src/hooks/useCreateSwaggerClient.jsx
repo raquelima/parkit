7afb2234cfd96e3bf9d70beb81b2e49f86817964
Raquel Lima <rrlima.raquel@gmail.com>
2023-03-16 12:21:33 +0100

    make swaggerClient hook saved user credentials

--- src/hooks/useCreateSwaggerClient.jsx
+++ src/hooks/useCreateSwaggerClient.jsx
@@ -3,30 +3,32 @@ import SwaggerClient from "swagger-client";
 import { developmentBaseUrl } from "../../mock/conf.js";
 import spec from "@berufsbildung-basel/parkit-spec/api.yml";
 
-function useCreateSwaggerClient() {
+function useCreateSwaggerClient(user) {
   const [swaggerClient, setSwaggerClient] = useState(null);
 
   useEffect(() => {
-    new SwaggerClient({
-      spec: {
-        ...spec,
-        servers: [
-          { url: `${developmentBaseUrl}/api`, description: "Test server" },
-        ],
-      },
-      requestInterceptor: (request) => {
-        Object.keys(localStorage).forEach((key) => {
-          if (key.startsWith("x-test-")) {
-            request.headers[key] = localStorage.getItem(key);
-          }
-        });
-        request.headers["authorization"] = `Basic ${btoa(
-          "test@adobe.com:testPassword"
-        )}`;
-        return request;
-      },
-    }).then((client) => setSwaggerClient(client));
-  }, []);
+    if (user) {
+      new SwaggerClient({
+        spec: {
+          ...spec,
+          servers: [
+            { url: `${developmentBaseUrl}/api`, description: "Test server" },
+          ],
+        },
+        requestInterceptor: (request) => {
+          Object.keys(localStorage).forEach((key) => {
+            if (key.startsWith("x-test-")) {
+              request.headers[key] = localStorage.getItem(key);
+            }
+          });
+          request.headers["authorization"] = `Basic ${btoa(
+            user.username + ":" + user.password
+          )}`;
+          return request;
+        },
+      }).then((client) => setSwaggerClient(client));
+    }
+  }, [user]);
 
   return swaggerClient;
 }
