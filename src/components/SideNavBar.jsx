ff7bb3fb353d8bdff1a3054d708288c551ac3bfc
Raquel Lima <rrlima.raquel@gmail.com>
2023-03-17 08:08:38 +0100

    add some styling overall

--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -2,6 +2,7 @@ import { Drawer, Toolbar, Typography, Divider } from "@mui/material";
 import { DRAWERWIDTH } from "../Constants";
 import { THEMECOLOR } from "../Constants";
 import SideNavBarList from "./SideNavBarList";
+import logo from "../assets/adobe.png";
 
 function SideNavBar() {
   return (
@@ -19,8 +20,17 @@ function SideNavBar() {
       variant="permanent"
       anchor="left"
     >
-      <Toolbar variant="dense" sx={{ minHeight: 44 }}>
-        <Typography>ParkIT</Typography>
+      <Toolbar
+        variant="dense"
+        sx={{
+          minHeight: 44,
+        }}
+      >
+        <img src={logo} width={23} height={20} />
+        <Typography variant="h5" sx={{ fontWeight: "300", px: 1 }}>
+          l
+        </Typography>
+        <Typography variant="h6">ParkIt</Typography>
       </Toolbar>
       <Divider />
       <SideNavBarList />
