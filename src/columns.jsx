8d3d74538b6334156a2979d77c0fef057b44af5d
Raquel Lima <rrlima.raquel@gmail.com>
2023-03-17 09:50:51 +0100

    make cancel button only render for upcoming reservations

--- src/columns.jsx
+++ src/columns.jsx
@@ -1,7 +1,95 @@
 import { format } from "date-fns";
-import { IconButton} from "@mui/material";
+import { IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import StatusChip from "./components/StatusChip";
+
+const now = format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+export const reservationsColumns = [
+  {
+    field: "date",
+    headerName: "Date",
+    flex: 1,
+    width: 200,
+    valueGetter: (reservations) =>
+      format(new Date(reservations.row.date), "dd/MM/yyyy"),
+  },
+  {
+    field: "duration",
+    headerName: "Duration",
+    flex: 1,
+    sortable: false,
+    width: 200,
+    valueGetter: (reservations) =>
+      `${format(new Date(reservations.row.start_time), "hh:mm")} - ${format(
+        new Date(reservations.row.end_time),
+        "hh:mm"
+      )}`,
+  },
+  {
+    field: "vehicle",
+    headerName: "Vehicle",
+    flex: 1,
+    sortable: false,
+    width: 200,
+    valueGetter: (reservations) =>
+      `${reservations.row.vehicle.make} ${reservations.row.vehicle.model}`,
+  },
+  {
+    field: "plateNumber",
+    headerName: "Plate number",
+    flex: 1,
+    sortable: false,
+    width: 200,
+    valueGetter: (reservations) =>
+      reservations.row.vehicle.license_plate_number,
+  },
+  {
+    field: "parking_spot_id",
+    headerName: "Parking spot",
+    flex: 1,
+    sortable: false,
+    width: 200,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    flex: 1,
+    sortable: false,
+    width: 200,
+    renderCell: (reservations) => {
+      let status;
+      const cancelled = reservations.row.cancelled;
+      const startTime = reservations.row.start_time;
+
+      cancelled
+        ? (status = "cancelled")
+        : now > startTime
+        ? (status = "upcoming")
+        : (status = "overdue");
+
+      return <StatusChip status={status} />;
+    },
+  },
+  {
+    field: "cancel",
+    headerName: " ",
+    sortable: false,
+    width: 70,
+    renderCell: (reservations) => {
+      const startTime = reservations.row.start_time;
+      const upcoming = now > startTime ? true : false;
+
+      if (upcoming) {
+        return (
+          <IconButton aria-label="cancel reservation" color="error">
+            <CloseIcon />
+          </IconButton>
+        );
+      }
+    },
+  },
+];
 
 export const upcomingReservationsColumns = [
   {
@@ -60,81 +148,3 @@ export const upcomingReservationsColumns = [
     },
   },
 ];
-
-export const reservationsColumns = [
-  {
-    field: "date",
-    headerName: "Date",
-    flex: 1,
-    width: 200,
-    valueGetter: (reservations) =>
-      format(new Date(reservations.row.date), "dd/MM/yyyy"),
-  },
-  {
-    field: "duration",
-    headerName: "Duration",
-    flex: 1,
-    sortable: false,
-    width: 200,
-    valueGetter: (reservations) =>
-      `${format(new Date(reservations.row.start_time), "hh:mm")} - ${format(
-        new Date(reservations.row.end_time),
-        "hh:mm"
-      )}`,
-  },
-  {
-    field: "vehicle",
-    headerName: "Vehicle",
-    flex: 1,
-    sortable: false,
-    width: 200,
-    valueGetter: (reservations) =>
-      `${reservations.row.vehicle.make} ${reservations.row.vehicle.model}`,
-  },
-  {
-    field: "plateNumber",
-    headerName: "Plate number",
-    flex: 1,
-    sortable: false,
-    width: 200,
-    valueGetter: (reservations) =>
-      reservations.row.vehicle.license_plate_number,
-  },
-  {
-    field: "parking_spot_id",
-    headerName: "Parking spot",
-    flex: 1,
-    sortable: false,
-    width: 200,
-  },
-  {
-    field: "status",
-    headerName: "Status",
-    flex: 1,
-    sortable: false,
-    width: 200,
-    renderCell: (reservations) => {
-      return <StatusChip startTime={reservations.row.start_time} cancelled={reservations.row.cancelled}/>;
-    },
-  },
-  {
-    field: "cancel",
-    headerName: " ",
-    sortable: false,
-    width: 70,
-    renderCell: (cellValues) => {
-      return (
-        <IconButton
-          aria-label="cancel reservation"
-          color="error"
-          onClick={(event) => {
-            handleClick(event, cellValues);
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-      );
-    },
-  },
-];
-
