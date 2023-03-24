9915a1a58065d210559ceab123e4e82353b95280
Raquel Lima <rrlima.raquel@gmail.com>
2023-03-24 12:07:50 +0100

    comment all files in utils file

--- src/utils/filterUpcomingReservations.jsx
+++ src/utils/filterUpcomingReservations.jsx
@@ -1,8 +1,12 @@
 import { format } from "date-fns";
 
-const now = format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-
+/**
+ * @description Filters an array of reservations and filters the upcoming ones
+ * @param {Array} reservations
+ * @returns {Array} An array of upcoming reservations
+ */
 function filterUpcomingReservations(reservations) {
+  const now = format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
   return reservations?.filter(
     (reservation) => reservation.start_time > now && !reservation.cancelled
   );
